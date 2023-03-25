cd5cd20f8065411fa129503b4b56b327b8c27f43
Mst Rinky Akter <mstrinkya@gmail.com>
2023-03-25 23:51:25 +0600

    radianToDegree

---
+++ problem2.js
@@ -0,0 +1,8 @@
+// radian to degree ----------
+
+function radianToDegree(radianNumber){
+    const degree = radianNumber * 180/3.1416;
+    // return degree;
+    console.log(degree);
+}
+radianToDegree(1);
