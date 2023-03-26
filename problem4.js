97b1dd51d78a546267bfb11824e855a6c95f43b1
Mst Rinky Akter <mstrinkya@gmail.com>
2023-03-26 22:48:03 +0600

    factorial using for loop solving changed

--- problem4.js
+++ problem4.js
@@ -1,15 +1,15 @@
 // factorial ------------------
-// let factorial = 1;
-// for (let i=6; i>1; i++){
-//   factorial = factorial *i;
-// }
-// console.log(factorial);
-
-function factorial(i){
-  if( i== 1 ){
-    return 1;
-  }
-  return i * factorial(i-1);
+let factorial = 1;
+for (let i=5; i>1; i--){
+  factorial = factorial *i;
 }
+console.log(factorial);
 
-console.log( factorial(5));
+// function factorial(i){
+//   if( i== 1 ){
+//     return 1;
+//   }
+//   return i * factorial(i-1);
+// }
+
+// console.log( factorial(5));
