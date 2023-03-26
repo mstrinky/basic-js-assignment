4459116c180be50956acb5f68187f8ee41baa236
Mst Rinky Akter <mstrinkya@gmail.com>
2023-03-26 22:45:41 +0600

    factorial using function

--- problem4.js
+++ problem4.js
@@ -1,7 +1,15 @@
 // factorial ------------------
-let factorial = 1;
-for (let i=6; i>1; i++){
-  factorial = factorial *i;
-}
-console.log(factorial);
+// let factorial = 1;
+// for (let i=6; i>1; i++){
+//   factorial = factorial *i;
+// }
+// console.log(factorial);
 
+function factorial(i){
+  if( i== 1 ){
+    return 1;
+  }
+  return i * factorial(i-1);
+}
+
+console.log( factorial(5));
