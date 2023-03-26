824e6054cabfb25a4ed7ffe280490fd9fa77ea32
Mst Rinky Akter <mstrinkya@gmail.com>
2023-03-26 22:29:00 +0600

    sum of some numbers using function

--- problem3.js
+++ problem3.js
@@ -1,5 +1,13 @@
-let sum=0;
-for  (let i=9; i>1; i--){
-    sum = sum +i;
+// let sum=0;
+// for  (let i=9; i>1; i--){
+//     sum = sum +i;
+// }
+// console.log(sum);
+
+function sum(i){
+    if(i===1){
+        return 1;
+    }
+    return i + sum(i-1);
 }
-console.log(sum);
+console.log(sum(7));
