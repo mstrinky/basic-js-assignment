747e6df178a1214a7d74bbf5987d071dd5113abd
Mst Rinky Akter <mstrinkya@gmail.com>
2023-03-26 22:42:02 +0600

    factorial using for loop

--- problem3.js
+++ problem3.js
@@ -1,3 +1,5 @@
+// recurtion ---------------
+
 // let sum=0;
 // for  (let i=9; i>1; i--){
 //     sum = sum +i;
---
+++ problem4.js
@@ -0,0 +1,7 @@
+// factorial ------------------
+let factorial = 1;
+for (let i=6; i>1; i++){
+  factorial = factorial *i;
+}
+console.log(factorial);
+
